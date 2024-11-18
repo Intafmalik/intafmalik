fd2a4a0242e8fb2641c6bf2189c33623e9430122
Intaf Malik <intafmalik731@gmail.com>
2024-11-18 19:46:25 +0545

    update index 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: '/index.html', // Ensure correct input file
+    },
+    outDir: 'dist', // Output directory for build files
+    assetsDir: 'assets', // Directory for assets
+  },
+});
